da043b33e7d6d9b36deb07fb431b7deba90cbc89
Cheburator <sashadanilenko06@gmail.com>
2023-11-21 13:22:56 +0100

    email verification added

--- src/schemas.py
+++ src/schemas.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
 
 # Contacts 
@@ -58,3 +58,7 @@ class TokenModel(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+
+class RequestEmail(BaseModel):
+    email: EmailStr
